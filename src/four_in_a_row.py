2a5b4c55dde565854ec2eef69610543d6708a080
Olof Bjarnson <olof.bjarnason@gmail.com>
2020-10-02 07:25:26 +0200

    structure production code

--- src/four_in_a_row.py
+++ src/four_in_a_row.py
@@ -1,11 +1,18 @@
 from collections import defaultdict
 
-
+# CONSTANTS #
 RED, YELLOW, EMPTY = range(3)
 
 
-def print_color(color):
-    return 'red' if color == RED else 'yellow'
+# STATES #
+
+class GameState(object):
+    def __init__(self):
+        self.board = defaultdict(lambda: EMPTY)
+        self.whos_turn_is_it = RED
+
+    def whos_turn(self):
+        return print_color(self.whos_turn_is_it)
 
 
 class GameOverState(object):
@@ -13,23 +20,14 @@ class GameOverState(object):
         self.winner = winner
 
 
-def extract(board, pos, dir):
-    cells = []
-    for i in range(4):
-        p = (pos[0] + dir[0] * i, pos[1] + dir[1] * i)
-        cells.append(board[p])
-    return cells
+# MESSAGES #
+
+class ColumnWasClicked(object):
+    def __init__(self, column):
+        self.column = column
 
 
-def check_winning_state(board, color):
-    for y in range(6):
-        for x in range(7):
-            for dir in [(0, 1), (1, 0), (1, 1)]:
-                cells = extract(board, (x, y), dir)
-                if all(cell == color for cell in cells):
-                    return True
-    return False
-
+# FUNCTIONS #
 
 def update(model, msg):
     if isinstance(model, GameState):
@@ -52,15 +50,23 @@ def place_brick(board, color, column):
     return board
 
 
-class GameState(object):
-    def __init__(self):
-        self.board = defaultdict(lambda: EMPTY)
-        self.whos_turn_is_it = RED
-
-    def whos_turn(self):
-        return print_color(self.whos_turn_is_it)
+def print_color(color):
+    return 'red' if color == RED else 'yellow'
 
 
-class ColumnWasClicked(object):
-    def __init__(self, column):
-        self.column = column
+def extract(board, pos, dir):
+    cells = []
+    for i in range(4):
+        p = (pos[0] + dir[0] * i, pos[1] + dir[1] * i)
+        cells.append(board[p])
+    return cells
+
+
+def check_winning_state(board, color):
+    for y in range(6):
+        for x in range(7):
+            for dir in [(0, 1), (1, 0), (1, 1)]:
+                cells = extract(board, (x, y), dir)
+                if all(cell == color for cell in cells):
+                    return True
+    return False
